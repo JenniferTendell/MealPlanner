70528ee28e792f20a450a6d1788a21112e812af9
Jennifer Tendell <jennifer.tendell@gmail.com>
2021-09-19 17:54:23 +0200

    Add recipe file.

---
+++ src/assets/recipes.js
@@ -0,0 +1,40 @@
+/* Template 
+{
+        id: 0,
+        title: 'Titel',
+        image: 'https://images.unsplash.com/photo-1604152135912-04a022e23696?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80',
+        ingredients: [
+            {
+                amount: [3],
+                unit: ['dl'],
+                ingredient: ['Mjölk'],
+            }
+        ],
+    },
+*/
+
+export const recipes = [
+    {
+        id: 0,
+        title: 'Titel',
+        image: 'https://images.unsplash.com/photo-1604152135912-04a022e23696?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80',
+        ingredients: [
+            {
+                amount: [3],
+                unit: ['dl'],
+                ingredient: ['Mjölk'],
+            }
+        ],
+    }, {
+        id: 0,
+        title: 'Titel',
+        image: 'https://images.unsplash.com/photo-1604152135912-04a022e23696?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80',
+        ingredients: [
+            {
+                amount: [500],
+                unit: ['g'],
+                ingredient: ['Pasta'],
+            }
+        ],
+    },
+]
